296d166c637994c784f6c4db59d269ed1acec05d
Alex Jose Zumaeta <alex.loja@utec.edu.pe>
2021-08-02 23:28:27 -0500

    mobile

--- config.py
+++
@@ -1,6 +0,0 @@
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:1234567@localhost:5432/proyectodbp"
-
-#QLALCHEMY_DATABASE_URI = "postgresql://sec01group01:utec2021@204.2.195.90:30794/CS2901sec01"
-TESTING = True
-SECRET_KEY = "FDsarfewaifhq34985ty3hfg043y2th43wef"
